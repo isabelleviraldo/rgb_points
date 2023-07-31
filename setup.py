ba18ec8fa90b5278642154b9d35e30c05889a524
Isabelle Viraldo <108153804+isabelleviraldo@users.noreply.github.com>
2023-07-31 11:30:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'color_mapping'
+package_name = 'rgb_images'
 
 setup(
     name=package_name,
@@ -20,14 +20,14 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'read_pcl = color_mapping.reading_point_cloud:main'
-            'read_img = color_mapping.reading_image:main'
-            'read_obj = color_mappint.reading_clusters:main'
-            'test_math = color_mapping.math:main'
-            'color_points_v1 = color_mapping.initial_attempt:main'
-            'color_points_new_math = color_mapping.fixed_math:main'
-            'color_points_center = color_mapping.color_objects_using_center:main'
-            'garbage_fire_testing = color_mapping.garbage_fire:main'
+            'read_pcl = rgb_images.reading_point_cloud:main'
+            'read_img = rgb_images.reading_image:main'
+            'read_obj = rgb_images.reading_clusters:main'
+            'test_math = rgb_images.math:main'
+            'color_points_v1 = rgb_images.initial_attempt:main'
+            'color_points_new_math = rgb_images.fixed_math:main'
+            'color_points_center = rgb_images.color_objects_using_center:main'
+            'garbage_fire_testing = rgb_images.garbage_fire:main'
         ],
     },
 )
